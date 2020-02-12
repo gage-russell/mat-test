44c78ee0d97e6f9edaf57bc83467d28e8b5feeb0
gage-russell <grussell@jp3measurement.com>
2020-02-11 21:16:24 -0600

    test

---
+++ install.m
@@ -0,0 +1,9 @@
+function install
+% Generated with Toolbox Extender https://github.com/ETMC-Exponenta/ToolboxExtender
+dev = Dev;
+dev.test('', false);
+% Post-install commands
+cd('..');
+ext = Extender;
+ext.doc;
+% Add your post-install commands below
